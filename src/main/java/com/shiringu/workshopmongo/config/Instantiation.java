e87827181476503081fd59d3b47fbcbed518fa5a
Gabriel <gabrielffloriano1@gmail.com>
2024-05-08 09:38:05 -0300

    Projeção dos dados do autor com DTO

--- src/main/java/com/shiringu/workshopmongo/config/Instantiation.java
+++ src/main/java/com/shiringu/workshopmongo/config/Instantiation.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Configuration;
 
 import com.shiringu.workshopmongo.domain.Post;
 import com.shiringu.workshopmongo.domain.User;
+import com.shiringu.workshopmongo.dto.AuthorDTO;
 import com.shiringu.workshopmongo.repository.PostRepository;
 import com.shiringu.workshopmongo.repository.UserRepository;
 
@@ -35,10 +36,11 @@ public class Instantiation implements CommandLineRunner {
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
 		
-		Post p1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para SP",maria);
-		Post p2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!",maria);
+		userRepository.saveAll(Arrays.asList(maria, alex, bob)); //é preciso salvar p/ criar os ids
+
+		Post p1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para SP",new AuthorDTO(maria));
+		Post p2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!",new AuthorDTO(maria));
 		
-		userRepository.saveAll(Arrays.asList(maria, alex, bob));
 		postRepository.saveAll(Arrays.asList(p1,p2));
 	}
 
