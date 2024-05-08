c34391da9e04fcdf3c629d53c001ca6897a9f6ee
Gabriel <gabrielffloriano1@gmail.com>
2024-05-08 09:27:45 -0300

    Criando entity Post com User aninhado

---
+++ src/main/java/com/shiringu/workshopmongo/repository/PostRepository.java
@@ -0,0 +1,11 @@
+package com.shiringu.workshopmongo.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.shiringu.workshopmongo.domain.Post;
+
+@Repository
+public interface PostRepository extends MongoRepository<Post, String>{
+	
+}
